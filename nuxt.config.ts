859f64c7dd091ca61bb5a9645d4dda6517a94b48
Antony <antony.huart37@gmail.com>
2026-04-20 16:14:45 +0200

    clean repo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxtjs/google-fonts',
+  ],
+
+  googleFonts: {
+    families: {
+      'Cormorant Garamond': [300, 400, 600],
+      'Didact Gothic': [400],
+    },
+    display: 'swap',
+  },
+
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+  },
+
+  css: ['~/assets/css/main.css'],
 })
